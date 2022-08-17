2172eee9e0ace91b11d48ecd0d7cf1841359eba9
rodrigorunner <rodrigoregisrun@gmail.com>
2022-08-17 10:43:37 -0300

    função de middleware adicionada

--- CRUD/MERN/coach/backend/server.js
+++ CRUD/MERN/coach/backend/server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const {erroHandler} = require('./middleware/errorMiddleware')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.use('/api/v1/', require('./routes/routesList'))
 
+app.use(erroHandler)
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
