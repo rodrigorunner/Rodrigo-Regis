67912ba250c0b852fafc54694ed9e36cb730f5fd
rodrigorunner <rodrigoregisrun@gmail.com>
2022-08-17 10:43:57 -0300

    tratamento de erro adicionado

--- CRUD/MERN/coach/backend/controller/listController.js
+++ CRUD/MERN/coach/backend/controller/listController.js
@@ -1,18 +1,25 @@
-const getList = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getList = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get Post'})
-}
+})
 
-const createList = (req, res) => {
+const createList = asyncHandler(async (req, res) => {
+    if(!req.body.nome || !req.body.sobrenome || !req.body.email || !req.body.idade) {
+        res.status(400)
+        throw new Error('Please add a text.')
+    }
+
     res.status(200).json({message: 'Create Post'})
-}
+})
 
-const updateList = (req, res) => {
+const updateList = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update Post ${req.params.id}`})
-}
+})
 
-const deleteList = (req, res) => {
+const deleteList = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete Post ${req.params.id}`})
-}
+})
 
 module.exports = {
     getList,
